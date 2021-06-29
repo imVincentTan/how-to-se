da6a0740ce4606cd9d54b63ef204561028ecb096
Vincent <vincent.tan.1998@gmail.com>
2021-06-29 13:24:20 -0400

    mapbox

--- myapp/src/app/map-sample/map-sample.component.ts
+++ myapp/src/app/map-sample/map-sample.component.ts
@@ -1,18 +1,12 @@
 import { environment } from 'src/environments/environment';
 import { Component, OnInit } from '@angular/core';
 
-
+// mapbox requirements
 import 'mapbox-gl/dist/mapbox-gl.css';
-
 // import mapboxgl from 'mapbox-gl';
 const mapboxgl = require('mapbox-gl');
-
-
-declare var require: any
-// import * as mapboxgl from 'mapbox-gl';
-
-// import { Map } from "mapbox-gl/dist/mapbox-gl"
-// import * as mapboxgl from "mapbox-gl/dist/mapbox-gl"
+// not quite sure why this fixes it but...
+declare var require: any;
 
 
 @Component({
@@ -22,25 +16,30 @@ declare var require: any
 })
 export class MapSampleComponent implements OnInit {
 
-  // map: mapboxgl.Map;
   style = 'mapbox://styles/mapbox/streets-v11';
-  lat = 37.75;
-  lng = -122.41;
+  lat = 49.25;
+  lng = -123.06;
   
   constructor() { }
 
   ngOnInit(): void {
 
-    // mapboxgl.accessToken = environment.mapbox.accessToken;
-    mapboxgl.accessToken = 'pk.eyJ1IjoiaW12aW5jZW50IiwiYSI6ImNrcWI2ejdqZzBnZzYybnQ5anR0YmVzNHUifQ.WlcjU8wkH_k-eEw7DeqoOA';
+    mapboxgl.accessToken = environment.mapbox.accessToken;
     const map = new mapboxgl.Map({
       container: 'map', // container ID
-      style: 'mapbox://styles/mapbox/streets-v11', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
+      style: this.style, // style URL
+      center: [this.lng, this.lat], // starting position [lng, lat]
       zoom: 9 // starting zoom
     });
     // Add map controls
-    // this.map.addControl(new mapboxgl.NavigationControl());
+    map.addControl(new mapboxgl.NavigationControl());
+
+    // Set options
+    var marker = new mapboxgl.Marker({
+      color: "#FFFFFF",
+      draggable: false
+    }).setLngLat([-123.010208, 49.249228])
+    .addTo(map);
   }
 
 }
